2a6e4edf182db2eb307ea449771a20937d4198dc
Tyler Yahn <codingalias@gmail.com>
2022-09-13 08:07:33 -0700

    Initial transmute draft

--- transmute/trace.go
+++ transmute/trace.go
@@ -15,11 +15,199 @@
 package transmute
 
 import (
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/sdk/instrumentation"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	api "go.opentelemetry.io/otel/trace"
 )
 
-func Spans(otel []trace.ReadOnlySpan) ptrace.Traces {
-	// TODO
-	return ptrace.NewTraces()
+func Spans(s []trace.ReadOnlySpan) ptrace.Traces {
+	t := ptrace.NewTraces()
+	rMap := mapSpans(s)
+
+	rs := t.ResourceSpans()
+	rs.EnsureCapacity(len(rMap))
+	for res, sMap := range rMap {
+		r := rs.AppendEmpty()
+		r.SetSchemaUrl(res.SchemaURL())
+		setAttrMapIter(r.Resource().Attributes(), res.Iter())
+		setScopeSpans(r.ScopeSpans(), sMap)
+	}
+	return t
+}
+
+type scopeMap map[instrumentation.Scope][]trace.ReadOnlySpan
+
+type resMap map[resource.Resource]scopeMap
+
+func mapSpans(spans []trace.ReadOnlySpan) resMap {
+	if len(spans) == 0 {
+		return nil
+	}
+
+	rMap := make(resMap)
+	for _, s := range spans {
+		sMap := rMap[*s.Resource()]
+		if sMap == nil {
+			sMap = make(scopeMap)
+		}
+		roSpans := sMap[s.InstrumentationScope()]
+		roSpans = append(roSpans, s)
+		sMap[s.InstrumentationScope()] = roSpans
+		rMap[*s.Resource()] = sMap
+	}
+	return rMap
+}
+
+func setAttrMapIter(p pcommon.Map, o attribute.Iterator) {
+	p.EnsureCapacity(o.Len())
+	for o.Next() {
+		a := o.Attribute()
+		setAttribute(p, a)
+	}
+}
+
+func setAttrMapSlice(p pcommon.Map, o []attribute.KeyValue) {
+	p.EnsureCapacity(len(o))
+	for _, a := range o {
+		setAttribute(p, a)
+	}
+}
+
+func setAttribute(p pcommon.Map, a attribute.KeyValue) {
+	switch a.Value.Type() {
+	case attribute.BOOL:
+		p.UpsertBool(string(a.Key), a.Value.AsBool())
+	case attribute.INT64:
+		p.UpsertInt(string(a.Key), a.Value.AsInt64())
+	case attribute.FLOAT64:
+		p.UpsertDouble(string(a.Key), a.Value.AsFloat64())
+	case attribute.STRING:
+		p.UpsertString(string(a.Key), a.Value.AsString())
+	case attribute.BOOLSLICE:
+		s := p.UpsertEmptySlice(string(a.Key))
+		vSlice := a.Value.AsBoolSlice()
+		s.EnsureCapacity(len(vSlice))
+		for _, v := range vSlice {
+			s.AppendEmpty().SetBoolVal(v)
+		}
+	case attribute.INT64SLICE:
+		s := p.UpsertEmptySlice(string(a.Key))
+		vSlice := a.Value.AsInt64Slice()
+		s.EnsureCapacity(len(vSlice))
+		for _, v := range vSlice {
+			s.AppendEmpty().SetIntVal(v)
+		}
+	case attribute.FLOAT64SLICE:
+		s := p.UpsertEmptySlice(string(a.Key))
+		vSlice := a.Value.AsFloat64Slice()
+		s.EnsureCapacity(len(vSlice))
+		for _, v := range vSlice {
+			s.AppendEmpty().SetDoubleVal(v)
+		}
+	case attribute.STRINGSLICE:
+		s := p.UpsertEmptySlice(string(a.Key))
+		vSlice := a.Value.AsStringSlice()
+		s.EnsureCapacity(len(vSlice))
+		for _, v := range vSlice {
+			s.AppendEmpty().SetStringVal(v)
+		}
+	default:
+		// drop unknown.
+	}
+}
+
+func setScopeSpans(p ptrace.ScopeSpansSlice, o scopeMap) {
+	p.EnsureCapacity(len(o))
+	for scope, spans := range o {
+		scopeSpans := p.AppendEmpty()
+		setScope(scopeSpans.Scope(), scope)
+		setSpans(scopeSpans.Spans(), spans)
+	}
+}
+
+func setScope(p pcommon.InstrumentationScope, o instrumentation.Scope) {
+	p.SetName(o.Name)
+	p.SetVersion(o.Version)
+	// TODO: support scope attributes when added to o.
+}
+
+func setSpans(p ptrace.SpanSlice, o []trace.ReadOnlySpan) {
+	p.EnsureCapacity(len(o))
+	for _, s := range o {
+		setSpan(p.AppendEmpty(), s)
+	}
+}
+
+func setSpan(p ptrace.Span, o trace.ReadOnlySpan) {
+	p.SetName(o.Name())
+	p.SetTraceID(pcommon.NewTraceID(o.SpanContext().TraceID()))
+	p.SetSpanID(pcommon.NewSpanID(o.SpanContext().SpanID()))
+	p.SetTraceState(ptrace.TraceState(o.SpanContext().TraceState().String()))
+	p.SetParentSpanID(pcommon.NewSpanID(o.Parent().SpanID()))
+	p.SetKind(convertSpanKind(o.SpanKind()))
+	p.SetStartTimestamp(pcommon.NewTimestampFromTime(o.StartTime()))
+	p.SetEndTimestamp(pcommon.NewTimestampFromTime(o.EndTime()))
+	setAttrMapSlice(p.Attributes(), o.Attributes())
+	setLinks(p.Links(), o.Links())
+	setEvents(p.Events(), o.Events())
+	setStatus(p.Status(), o.Status())
+	p.SetDroppedAttributesCount(uint32(o.DroppedAttributes()))
+	p.SetDroppedLinksCount(uint32(o.DroppedLinks()))
+	p.SetDroppedEventsCount(uint32(o.DroppedEvents()))
+}
+
+func convertSpanKind(o api.SpanKind) ptrace.SpanKind {
+	switch o {
+	case api.SpanKindInternal:
+		return ptrace.SpanKindInternal
+	case api.SpanKindServer:
+		return ptrace.SpanKindServer
+	case api.SpanKindClient:
+		return ptrace.SpanKindClient
+	case api.SpanKindProducer:
+		return ptrace.SpanKindProducer
+	case api.SpanKindConsumer:
+		return ptrace.SpanKindConsumer
+	}
+	return ptrace.SpanKindUnspecified
+}
+
+func setLinks(p ptrace.SpanLinkSlice, o []trace.Link) {
+	p.EnsureCapacity(len(o))
+	for _, ol := range o {
+		pl := p.AppendEmpty()
+		pl.SetTraceID(pcommon.NewTraceID(ol.SpanContext.TraceID()))
+		pl.SetSpanID(pcommon.NewSpanID(ol.SpanContext.SpanID()))
+		pl.SetTraceState(ptrace.TraceState(ol.SpanContext.TraceState().String()))
+		setAttrMapSlice(pl.Attributes(), ol.Attributes)
+		pl.SetDroppedAttributesCount(uint32(ol.DroppedAttributeCount))
+	}
+}
+
+func setEvents(p ptrace.SpanEventSlice, o []trace.Event) {
+	p.EnsureCapacity(len(o))
+	for _, oe := range o {
+		pe := p.AppendEmpty()
+		pe.SetName(oe.Name)
+		pe.SetTimestamp(pcommon.NewTimestampFromTime(oe.Time))
+		setAttrMapSlice(pe.Attributes(), oe.Attributes)
+		pe.SetDroppedAttributesCount(uint32(oe.DroppedAttributeCount))
+	}
+}
+
+func setStatus(p ptrace.SpanStatus, o trace.Status) {
+	p.SetMessage(o.Description)
+	switch o.Code {
+	case codes.Ok:
+		p.SetCode(ptrace.StatusCodeOk)
+	case codes.Error:
+		p.SetCode(ptrace.StatusCodeError)
+	default:
+		p.SetCode(ptrace.StatusCodeUnset)
+	}
 }
